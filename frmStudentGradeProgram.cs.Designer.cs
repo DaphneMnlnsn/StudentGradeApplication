aa4433f94c24ba3b38d831aa4599c63def33a8a2
DaphneMnlnsn <daphnemanalansan1213@gmail.com>
2024-02-23 08:51:32 +0800

    Add project files.

---
+++ frmStudentGradeProgram.cs.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace StudentGradeApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.grade1Input = new System.Windows.Forms.TextBox();
+            this.grade2Input = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.grade3Input = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.grade4Input = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.grade5Input = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.passOrFail = new System.Windows.Forms.Label();
+            this.showAvg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(76, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(357, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(199, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Grades";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "English";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // grade1Input
+            // 
+            this.grade1Input.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grade1Input.Location = new System.Drawing.Point(91, 97);
+            this.grade1Input.Name = "grade1Input";
+            this.grade1Input.Size = new System.Drawing.Size(62, 25);
+            this.grade1Input.TabIndex = 4;
+            // 
+            // grade2Input
+            // 
+            this.grade2Input.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grade2Input.Location = new System.Drawing.Point(371, 94);
+            this.grade2Input.Name = "grade2Input";
+            this.grade2Input.Size = new System.Drawing.Size(62, 25);
+            this.grade2Input.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(309, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Math";
+            // 
+            // grade3Input
+            // 
+            this.grade3Input.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grade3Input.Location = new System.Drawing.Point(90, 145);
+            this.grade3Input.Name = "grade3Input";
+            this.grade3Input.Size = new System.Drawing.Size(62, 25);
+            this.grade3Input.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Science";
+            // 
+            // grade4Input
+            // 
+            this.grade4Input.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grade4Input.Location = new System.Drawing.Point(371, 143);
+            this.grade4Input.Name = "grade4Input";
+            this.grade4Input.Size = new System.Drawing.Size(62, 25);
+            this.grade4Input.TabIndex = 10;
+            this.grade4Input.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(309, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Filipino";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // grade5Input
+            // 
+            this.grade5Input.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grade5Input.Location = new System.Drawing.Point(91, 198);
+            this.grade5Input.Name = "grade5Input";
+            this.grade5Input.Size = new System.Drawing.Size(62, 25);
+            this.grade5Input.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(20, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "History";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(250, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 42);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Generate Average";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // passOrFail
+            // 
+            this.passOrFail.AutoSize = true;
+            this.passOrFail.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold);
+            this.passOrFail.Location = new System.Drawing.Point(20, 252);
+            this.passOrFail.Name = "passOrFail";
+            this.passOrFail.Size = new System.Drawing.Size(0, 20);
+            this.passOrFail.TabIndex = 14;
+            // 
+            // showAvg
+            // 
+            this.showAvg.AutoSize = true;
+            this.showAvg.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold);
+            this.showAvg.Location = new System.Drawing.Point(20, 280);
+            this.showAvg.Name = "showAvg";
+            this.showAvg.Size = new System.Drawing.Size(0, 20);
+            this.showAvg.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(470, 338);
+            this.Controls.Add(this.showAvg);
+            this.Controls.Add(this.passOrFail);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grade5Input);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.grade4Input);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.grade3Input);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.grade2Input);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.grade1Input);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Student Grade Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox grade1Input;
+        private System.Windows.Forms.TextBox grade2Input;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox grade3Input;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox grade4Input;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox grade5Input;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label passOrFail;
+        private System.Windows.Forms.Label showAvg;
+    }
+}
+
